4cd2dbb5cf6b9cdb07b8e1a4c5e36d8a90155a58
Booker King <btking1@my.waketech.edu>
2022-08-28 14:10:02 -0400

    test about section

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";   
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () =>{
+    // renders About test
+
+    // 1st test
+    it('renders', () => {
+        render(<About />);
+    })
+
+    // 2nd test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
